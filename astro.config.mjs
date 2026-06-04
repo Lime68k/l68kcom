51a505a4aecb18358affaa9061504937530f7a02
Camille Amet <camille.ameti@gmail.com>
2026-06-04 14:03:41 +0200

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "VG5000",
+        cssVariable: "--font-vg",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/font.otf'],
+                weight: 'normal',
+                style: 'normal'
+            }]
+        }
+    }]
+});
