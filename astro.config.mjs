60975a265bb89d8d48c60f510598ee2f5032df0a
Camille Amet <camille.ameti@gmail.com>
2026-06-06 02:50:42 +0200

    output ocnfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: './publish',
     fonts: [{
         provider: fontProviders.local(),
         name: "VG5000",
