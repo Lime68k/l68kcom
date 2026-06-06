515db5fd60423ea1e9af601734a3eba9ea239de1
Camille Amet <camille.ameti@gmail.com>
2026-06-06 03:01:54 +0200

    slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lime68k.github.io/',
-    base: '/l68kcom',
+    base: '/l68kcom/',
     fonts: [{
         provider: fontProviders.local(),
         name: "VG5000",
